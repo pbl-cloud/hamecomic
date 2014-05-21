24d7cb9b14eef894abf1353c7db6f50bc4b9ceba
tuvistavie <tuvistavie@gmail.com>
2014-05-22 02:25:44 +0900

    Modularize and simplify code.

--- lib/hamecomic.rb
+++ lib/hamecomic.rb
@@ -1,5 +1,24 @@
-require "hamecomic/version"
+require "opencv"
+require "./hamecomic/common"
+require "./hamecomic/comic_maker"
 
 module Hamecomic
-  # Your code goes here...
+  LIB_PATH  = File.expand_path(File.dirname(__FILE__))
+  DATA_PATH = File.join(LIB_PATH, 'data')
+  HAARCASCADE_CONFIG_FILENAME = 'haarcascade_frontalface_alt.xml'
+  HAARCASCADE_CONFIG_PATH = File.join(DATA_PATH,
+    "haarcascades", HAARCASCADE_CONFIG_FILENAME)
+
+  IMAGE_INFO = YAML::load_file(File.join(DATA_PATH, 'image_info.yml'))
+
+  @@images = IMAGE_INFO['images']
+
+  def make_comic(image, mode, options = {})
+    raise ArgumentError, 'invalid mode' unless @@images.has_key?(mode.to_s)
+    image = OpenCV::IplImage.load(image) unless image.is_a?(OpenCV::IplImage)
+    faces = detect_faces(image)
+    return [] if faces.empty?
+    comic_maker = ComicMaker.new(@@images[mode.to_s], options)
+    faces.map { |face| comic_maker.apply_transformation(face, options) }
+  end
 end
---
+++ lib/hamecomic/comic_maker.rb
@@ -0,0 +1,36 @@
+module Hamecomic
+  class ComicMaker
+    include Hamecomic
+    include OpenCV
+
+    def initialize(image_info, options = {})
+      @image = IplImage.load(File.join(DATA_PATH, image_info['filename']))
+      mask_path = image_info['mask_name'] || get_mask_path(image_info['filename'])
+      @mask  = IplImage.load(File.join(DATA_PATH, mask_path), CV_LOAD_IMAGE_GRAYSCALE)
+
+      @face_size = CvSize.new(image_info['width'], image_info['height'])
+      @face_rect = CvRect.new(image_info['left'], image_info['top'],
+        image_info['width'], image_info['height'])
+      @mask_rect = @mask.sub_rect(@face_rect)
+    end
+
+    def apply_transformation(image, options = {})
+      image = IplImage.load(image) if image.is_a?(String)
+      image = image.resize(@face_size)
+      image = apply_manga_effect(image)
+
+      new_image = @image.set_roi(@face_rect)
+      new_image = image.copy(new_image, @mask_rect)
+
+      new_image.reset_roi
+      new_image.save(options[:output]) if options[:output]
+      new_image
+    end
+
+    private
+    def get_mask_path(filename)
+      ext = File.extname(filename)
+      File.basename(filename, ext) + "_mask" + ext
+    end
+  end
+end
