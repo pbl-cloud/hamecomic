ffa3c3364b46de9ad60b2c76a60999a6cbaa2e10
tuvistavie <tuvistavie@gmail.com>
2014-05-22 04:00:12 +0900

    Improve CLI interface. Fix some bugs.

--- lib/hamecomic.rb
+++ lib/hamecomic.rb
@@ -1,5 +1,6 @@
 require "opencv"
 require "slop"
+require "yaml"
 
 require "hamecomic/common"
 require "hamecomic/comic_maker"
@@ -12,9 +13,9 @@ module Hamecomic
   HAARCASCADE_CONFIG_PATH = File.join(DATA_PATH,
     "haarcascades", HAARCASCADE_CONFIG_FILENAME)
 
-  IMAGE_INFO = YAML::load_file(File.join(DATA_PATH, 'image_info.yml'))
+  IMAGE_INFO = ::YAML::load_file(File.join(DATA_PATH, 'image_info.yml'))
 
-  @@images = IMAGE_INFO['images']
+  IMAGES = IMAGE_INFO['images']
 
   # Returns an array of IplImage with faces in comics.
   # Mode can be any image which has a key in 'image_info.yml'.
@@ -23,14 +24,15 @@ module Hamecomic
   # If the output name contains '%d', it will be replaced by
   # the image number.
   def make_comic(image, mode, options = {})
-    raise ArgumentError, 'invalid mode' unless @@images.has_key?(mode.to_s)
+    raise ArgumentError, 'invalid mode' unless IMAGES.has_key?(mode.to_s)
     image = OpenCV::IplImage.load(image) unless image.is_a?(OpenCV::IplImage)
     faces = detect_faces(image)
     return [] if faces.empty?
-    comic_maker = ComicMaker.new(@@images[mode.to_s], options)
+    comic_maker = ComicMaker.new(IMAGES[mode.to_s], options)
+    puts faces.count
     faces.each_with_index.map do |face, i|
-      options[:output] = options[:output] % i if options[:output]
-      comic_maker.apply_transformation(face, options)
+      output = options[:output] % i if options[:output]
+      comic_maker.apply_transformation(face, options.merge(output: output))
     end
   end
 end
